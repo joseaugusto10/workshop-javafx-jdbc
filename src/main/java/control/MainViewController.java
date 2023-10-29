a7442917dd20c80f254a30c437d07b04b8fabdbb
Jose Augusto <jose_augusto2019@hotmail.com>
2023-10-29 15:50:30 -0300

    Main view controller

---
+++ src/main/java/control/MainViewController.java
@@ -0,0 +1,37 @@
+package control;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.util.ResourceBundle;
+
+import java.net.URL;
+
+public class MainViewController implements Initializable{
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL uri,ResourceBundle rb){
+
+    }
+
+}
